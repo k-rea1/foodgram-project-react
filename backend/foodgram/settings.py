187ead935c6c6b885d1e5a905d2b1ce70282bd78
Kirill Ushakov <kir.ushakov@mail.ru>
2023-02-24 19:26:46 +0300

    test workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
